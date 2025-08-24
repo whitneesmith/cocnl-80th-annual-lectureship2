ae7fae3f76cc6b8b85093daeb6cfcd7cc1e86eb5
whitneesmith <whitsmit919@gmail.com>
2025-08-23 21:02:45 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -11,17 +11,24 @@ import AdminDashboard from './pages/AdminDashboard';
 function App() {
   return (
     <Router>
-      <Layout>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/register-form" element={<RegisterForm />} />
-          <Route path="/hotel" element={<Hotel />} />
-          <Route path="/vendors" element={<Vendors />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </Layout>
+      <Routes>
+        {/* Admin route without Layout wrapper */}
+        <Route path="/admin" element={<AdminDashboard />} />
+        
+        {/* All other routes with Layout wrapper */}
+        <Route path="/*" element={
+          <Layout>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/register" element={<Register />} />
+              <Route path="/register-form" element={<RegisterForm />} />
+              <Route path="/hotel" element={<Hotel />} />
+              <Route path="/vendors" element={<Vendors />} />
+              <Route path="/contact" element={<Contact />} />
+            </Routes>
+          </Layout>
+        } />
+      </Routes>
     </Router>
   );
 }
