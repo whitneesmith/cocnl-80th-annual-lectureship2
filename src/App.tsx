80266de597589eed09c6eb72f91e3a9e09bb3ad6
whitneesmith <whitsmit919@gmail.com>
2025-08-23 20:15:29 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 import Home from './pages/Home';
+import Register from './pages/Register';
 import RegisterForm from './pages/RegisterForm';
+import Hotel from './pages/Hotel';
+import Vendors from './pages/Vendors';
+import Contact from './pages/Contact';
 import AdminDashboard from './pages/AdminDashboard';
 
 function App() {
   return (
     <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/register" element={<RegisterForm />} />
-        <Route path="/admin" element={<AdminDashboard />} />
-      </Routes>
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/register-form" element={<RegisterForm />} />
+          <Route path="/hotel" element={<Hotel />} />
+          <Route path="/vendors" element={<Vendors />} />
+          <Route path="/contact" element={<Contact />} />
+          <Route path="/admin" element={<AdminDashboard />} />
+        </Routes>
+      </Layout>
     </Router>
   );
 }
